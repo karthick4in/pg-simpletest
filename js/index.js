ef00eff729726c50cfa3b03b932916a6430d860f
karthick4in <karthick4in@gmail.com>
2015-05-07 22:31:28 +0530

    full change
    full chagne

--- js/index.js
+++ js/index.js
@@ -1,60 +1,85 @@
-/**
- * Copyright 2014 Kinvey, Inc.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * Common app functionality will be attached to the `app` namespace.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 var app = {
-  /**
-   * Application constructor.
-   */
-  initialize: function() {
-    this.bindEvents();
-  },
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"433864121656","ecb":"app.onNotificationGCM"});
 
-  /**
-   * Bind event listeners.
-   */
-  bindEvents: function() {
-    // Bind any events that are required on startup. Common events are: `load`,
-    // `deviceready`, `offline`, and `online`.
-    document.addEventListener('deviceready', app.onDeviceReady, false);
-  },
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-  /**
-   * The deviceready event handler.
-   */
-  onDeviceReady: function() {
-    // Initialize push.
-    push.initialize();
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-    // Initialize Kinvey. Paste your app key and secret below.
-    var promise = Kinvey.init({
-      appKey    : 'kid_ZJC8lvYDkl',
-      appSecret : '378b5f14a8694b109819c43ea0cdae66'
-    });
-    promise.then(function(activeUser) {
-      // The `Kinvey.init` function returns a promise which resolves to the
-      // active user data. If there is no active user, create one.
-      if(null === activeUser) {
-        return Kinvey.User.signup();
-      }
-      return activeUser;
-    }).then(function(activeUser) {
-      // Your app is now connected to Kinvey and has an active user.
-    });
-  },
+        console.log('Received Event: ' + id);
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Callback Success! Result = '+result)
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+                break;
+
+            case 'message':
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                break;
+
+            case 'error':
+                alert('GCM error = '+e.msg);
+                break;
+
+            default:
+                alert('An unknown GCM event has occurred');
+                break;
+        }
+    }
+
 };
