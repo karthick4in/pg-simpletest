4030f30450f5bedce0030251d28d6fb2c914cc3d
karthick4in <karthick4in@gmail.com>
2015-05-08 19:27:55 +0530

    ok completed
    nice

--- js/index.js
+++ js/index.js
@@ -67,7 +67,7 @@ var app = {
                 {
                     console.log("Regid " + e.regid);
                     alert('registration id = '+e.regid);
-					location.href= 'http://192.168.1.5:81/rnt/id.php?id='+e.regid;
+					location.href= 'http://192.168.1.5:81/rnt/bg_service/AndroidGoogleColudMessaging/gcm_server_php/register.php?id='+e.regid; 
                 }
                 break;
 
